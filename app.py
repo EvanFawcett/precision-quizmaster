d179857d69792ceb66c32d0333b6a93a9c9bcf93
Evan <efawcett16@gmail.com>
2024-01-14 18:17:17 -0500

    Added simple question/answer functionality

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template
-#import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def index(): #home
+def index(): 
     return render_template('index.html')  
-    #return "Hello World!"
 
-#if __name__ == '__main__':
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host="0.0.0.0", port=port, debug=False)
+@app.route('/create_quiz', methods=['POST'])
+def create_quiz():
+    if request.method =='POST':
+        #Retrieve data from the form
+        question = request.form.get('question')
+        answer1 = request.form.get('answer1')
+        answer2 = request.form.get('answer2')
+
+        # For now, just print the data and modify later
+        print(f"Question: {question}")
+        print(f"Answer 1: {answer1}")
+        print(f"Answer 2: {answer2}")
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
